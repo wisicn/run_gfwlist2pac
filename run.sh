d84fad307f845297b325a40bbeb4c099a501720e
Wissen Xue <wisicn@gmail.com>
2017-01-05 14:55:13 +0800

    add new new gfwlist2dnsmaq

--- run.sh
+++ run.sh
@@ -4,7 +4,9 @@ gfwlist2pac -i ./gfwlist.txt -f ./proxy-socks.pac -p 'SOCKS5 127.0.0.1:1080; SOC
 gfwlist2pac -i ./gfwlist.txt -f ./proxy.pac -p 'PROXY 127.0.0.1:8118; DIRECT;' --user-rule ./user_rule.txt
 gfwlist2pac -i ./gfwlist.txt -f ./wpad.dat -p 'PROXY 10.10.88.1:8118; DIRECT;' --user-rule ./user_rule.txt
 #generate the dnsmasq
-./gfwlist2dnsmasq.sh
+#./gfwlist2dnsmasq.sh
+#new dnsmasq
+python ./new_gfwlist2dnsmasq.py -i ./gfwlist.txt -f ./dnsmasq_list.conf  -d '127.0.0.1#5353' --user-rule ./user_rule.txt
 #workaround to replace amazon.com to bbc.co.uk
 sed -i 's/amazon.com/bbc.co.uk/g' ./proxy.pac ./proxy-socks.pac ./wpad.dat ./dnsmasq_list.conf
 sed -i 's/amazonaws.com/twimg.edgesuite.net/g' ./proxy.pac ./proxy-socks.pac ./wpad.dat ./dnsmasq_list.conf

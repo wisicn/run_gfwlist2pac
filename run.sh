a102731d32ba7145739612acaea113768042c3d8
Wissen XUE <xuezhijian@gmail.com>
2017-01-05 12:16:17 +0800

    add the -i for gfwlist2pac

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-wget --no-check-certificate https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt
-gfwlist2pac -f ./proxy-socks.pac -p 'SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080; DIRECT;' --user-rule ./user_rule.txt
-gfwlist2pac -f ./proxy.pac -p 'PROXY 127.0.0.1:8118; DIRECT;' --user-rule ./user_rule.txt
-gfwlist2pac -f ./wpad.dat -p 'PROXY 10.10.88.1:8118; DIRECT;' --user-rule ./user_rule.txt
+wget --no-check-certificate -O ./gfwlist.txt https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt
+gfwlist2pac -i ./gfwlist.txt -f ./proxy-socks.pac -p 'SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080; DIRECT;' --user-rule ./user_rule.txt
+gfwlist2pac -i ./gfwlist.txt -f ./proxy.pac -p 'PROXY 127.0.0.1:8118; DIRECT;' --user-rule ./user_rule.txt
+gfwlist2pac -i ./gfwlist.txt -f ./wpad.dat -p 'PROXY 10.10.88.1:8118; DIRECT;' --user-rule ./user_rule.txt
 #generate the dnsmasq
 #./gfwlist2dnsmasq.sh
 #workaround to replace amazon.com to bbc.co.uk
